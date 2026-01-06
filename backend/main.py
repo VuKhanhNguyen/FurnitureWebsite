06bc123eda7354c9ca792bd8e0f8f1b8949c5450
DoKhanh2104 <winwin020104@gmail.com>
2026-01-06 21:11:52 +0700

    finish dashboard

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,7 @@ from app.models import Base
 from app.routers.product_router import router as product_router
 from app.routers.user_router import router as user_router
 from app.routers.order_router import router as order_router
+from app.routers.dashboard_router import router as dashboard_router
 
 from app.routers.password_router import router as password_router
 from app.routers.auth_router import router as auth_router
@@ -43,6 +44,7 @@ app.add_middleware(
 app.include_router(product_router, prefix="/api")
 app.include_router(user_router, prefix="/api")
 app.include_router(order_router, prefix="/api")
+app.include_router(dashboard_router, prefix="/api")
 app.include_router(password_router, prefix="/api")
 app.include_router(auth_router, prefix="/api")
 app.include_router(category_router, prefix="/api")
